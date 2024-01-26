b95c907055cc77fed888a87d0c5997f1e93cd2da
tranhoandz98 <tranhoan.dz98@gmail.com>
2024-01-26 22:32:52 +0700

    fix url product

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -36,10 +36,10 @@ export const removeSpecialCharacter = (str: string) =>
   str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, '')
 
 export const generateNameId = ({ name, id }: { name: string; id: string }) => {
-  return removeSpecialCharacter(name).replace(/\s/g, '-') + `-i.${id}`
+  return removeSpecialCharacter(name).replace(/\s/g, '-') + `-i-${id}`
 }
 
 export const getIdFromNameId = (nameId: string) => {
-  const arr = nameId.split('-i.')
+  const arr = nameId.split('-i-')
   return arr[arr.length - 1]
 }
